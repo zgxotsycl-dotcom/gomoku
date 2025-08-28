cbf1d33e91b7fcfe2898095f4bf2c28400d5af4a
galaxymon <zgxotsycl@gmail.com>
2025-08-28 21:39:31 +0900

    BESTai

--- src/components/I18nProvider.tsx
+++ src/components/I18nProvider.tsx
@@ -1,10 +1,21 @@
 'use client';
 
 import { I18nextProvider } from 'react-i18next';
+import { ReactNode, useEffect } from 'react';
 import i18n from '@/i18n';
-import { ReactNode } from 'react';
 
-const I18nProvider = ({ children }: { children: ReactNode }) => {
+interface I18nProviderProps {
+  children: ReactNode;
+  lng: string;
+}
+
+const I18nProvider = ({ children, lng }: I18nProviderProps) => {
+  useEffect(() => {
+    if (i18n.language !== lng) {
+      i18n.changeLanguage(lng);
+    }
+  }, [lng]);
+
   return <I18nextProvider i18n={i18n}>{children}</I18nextProvider>;
 };
 
