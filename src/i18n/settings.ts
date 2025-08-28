8e0f48c15dd3d45c2424316f3cfaa8f7f87c9e37
galaxymon <zgxotsycl@gmail.com>
2025-08-28 21:42:06 +0900

    BESTai

--- src/i18n/settings.ts
+++ src/i18n/settings.ts
@@ -2,7 +2,7 @@ export const fallbackLng = 'en';
 export const languages = [fallbackLng, 'ko', 'ja'];
 export const defaultNS = 'translation';
 
-export function getOptions(lng = fallbackLng, ns = defaultNS) {
+export function getOptions(lng = fallbackLng, ns: string | string[] = defaultNS) {
   return {
     supportedLngs: languages,
     fallbackLng,
